410b4443f238c7ef0e0b34df54c9bac286fde807
Luis Rovirosa <luisrovirosa@gmail.com>
2017-01-31 12:52:41 +0100

    refactor extract constant

--- src/test/java/myservice/InMemorySingleSignOnRegistryShould.java
+++ src/test/java/myservice/InMemorySingleSignOnRegistryShould.java
@@ -8,11 +8,15 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class InMemorySingleSignOnRegistryShould {
+
+    public static final String AN_USERNAME = "luisrovirosa";
+    public static final String A_PASSWORD = "aPassword";
+
     @Test
     public void return_a_sso_token_when_register_new_session() {
         InMemorySingleSignOnRegistry registry = new InMemorySingleSignOnRegistry();
 
-        SSOToken ssoToken = registry.registerNewSession("luisrovirosa", "aPassword");
+        SSOToken ssoToken = registry.registerNewSession(AN_USERNAME, A_PASSWORD);
 
         assertThat(ssoToken, not(nullValue()));
     }
