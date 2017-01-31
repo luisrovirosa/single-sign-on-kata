ac23e5a5a0c5d138f462b22b8a3f69af275d8d19
Luis Rovirosa <luisrovirosa@gmail.com>
2017-01-31 09:26:24 +0100

    valid_SSO_token_say_hello

--- src/main/java/myservice/MyService.java
+++ src/main/java/myservice/MyService.java
@@ -13,7 +13,6 @@ public class MyService {
     }
 
     public Response handleRequest(Request request) {
-        // TODO: check request has a valid SSOToken
-        return new Response("hello " + request.getName() + "!");
+        return new Response("Hello " + request.getName() + "!");
     }
 }
--- src/test/java/myservice/MyServiceTest.java
+++ src/test/java/myservice/MyServiceTest.java
@@ -1,19 +1,28 @@
 package myservice;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
 import sso.Request;
 import sso.Response;
+import sso.SSOToken;
 import sso.SingleSignOnRegistry;
 
 public class MyServiceTest {
 
     @Test
-    public void invalid_SSO_token_is_rejected() {
-        MyService service = new MyService(null);
-        Response response = service.handleRequest(new Request("Foo", null));
-        assertNotEquals("hello Foo!", response.getText());
+    public void valid_SSO_token_say_hello() {
+        SingleSignOnRegistry registry = mock(SingleSignOnRegistry.class);
+        SSOToken validToken = new SSOToken();
+        when(registry.isValid(validToken)).thenReturn(true);
+        MyService service = new MyService(registry);
+
+        Response response = service.handleRequest(new Request("Luis", validToken));
+
+        assertEquals("Hello Luis!", response.getText());
     }
 }
